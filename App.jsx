b68c623dda1087bed428985235e71dbfcac2235f
Nirav Sanghani <sanghaninirav258@gmail.com>
2025-02-19 15:59:53 +0530

    React native hide splash screen

--- App.jsx
+++ App.jsx
@@ -1,7 +1,11 @@
 import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide(); // Hide splash screen after initialization
+  }, []);
   return (
     <View>
       <Text>App</Text>
